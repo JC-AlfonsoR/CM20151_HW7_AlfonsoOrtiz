1d48af8562da6cad24e1d5670989898fc9161582
carolinaog <c.ortiz84@uniandes.edu.co>
2015-04-30 09:41:16 -0500

    se anhade tiempo

--- P2_Campo_magnetico_solar/convertir_fecha.r
+++ P2_Campo_magnetico_solar/convertir_fecha.r
@@ -1,2 +1,12 @@
-MyData <- read.csv(file="Metodos2015/HW7/CM20151_HW7_AlfonsoOrtiz/times.csv", header=TRUE, sep=",")
-MyData <- read.csv(file="times.csv", header=TRUE, sep=",")
+library(lubridate)
+library(dplyr)
+library(tidyr)
+destfile <- "Metodos2015/HW7/CM20151_HW7_AlfonsoOrtiz/P2_Campo_magnetico_solar/times.csv"
+MyData <- read.csv(destfile, header=TRUE, sep=",", skiprow=0)
+
+data=data.frame(MyData)
+colnames(data) <- c("fecha","hora")
+dat2 <- data.frame(do.call(rbind, strsplit(as.vector(data$hora), split = "_")))
+one=dat2[1,2]
+dat1=data.frame(dat2[2])
+
